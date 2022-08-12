0e75557a91620ef799ce0e9b015d05e969df507c
Klaribot <25907774+klaribot@users.noreply.github.com>
2022-08-11 22:29:13 -0400

    Track all files, add new hack

--- paper/src/main/java/com/programmerdan/minecraft/simpleadminhacks/hacks/NewfriendAssist.java
+++ paper/src/main/java/com/programmerdan/minecraft/simpleadminhacks/hacks/NewfriendAssist.java
@@ -20,7 +20,6 @@ import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerKickEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.event.player.PlayerRespawnEvent;
-import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 
 /**
---
+++ paper/src/main/java/com/programmerdan/minecraft/simpleadminhacks/hacks/basic/RitualCannibalism.java
@@ -0,0 +1,114 @@
+package com.programmerdan.minecraft.simpleadminhacks.hacks.basic;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.Material;
+import net.kyori.adventure.text.Component;
+
+
+import com.programmerdan.minecraft.simpleadminhacks.SimpleAdminHacks;
+import com.programmerdan.minecraft.simpleadminhacks.framework.BasicHack;
+import com.programmerdan.minecraft.simpleadminhacks.framework.BasicHackConfig;
+import com.programmerdan.minecraft.simpleadminhacks.framework.autoload.AutoLoad;
+import com.programmerdan.minecraft.simpleadminhacks.framework.autoload.DataParser;
+
+public class RitualCannibalism extends BasicHack {
+	
+	// I LOVE THESE AUTOLOAD DECORATORS!!! SO GOOD! THANK YOU FOR THIS, LEGIT
+	@AutoLoad
+	private double smiteChance;
+
+	@AutoLoad
+	private double lootingChance;
+
+	// Need these parameters in parenthesis for guaranteeing the config value is a Material, NOT a String
+	@AutoLoad(processor = DataParser.MATERIAL)
+	private Material meatItem;
+
+	@AutoLoad
+	private String meatName;
+
+	@AutoLoad
+	private List<String> meatLore;
+
+	@AutoLoad(processor = DataParser.MATERIAL)
+	private Material heartItem;
+
+	@AutoLoad
+	private String heartName;
+
+	@AutoLoad
+	private List<String> heartLore;
+
+	// Constructor that constructs our hack
+	public RitualCannibalism(SimpleAdminHacks plugin, BasicHackConfig config) {
+		super(plugin, config);
+	}
+
+	// THERE'S EVEN A DECORATOR FOR HANDLING EVENTS AUTOMAGICALLY???
+	// HOLY CRAP JAVA SYNTAX (and ESPECIALLY the SAH Framework) IS BASED
+	@EventHandler
+	public void onPlayerDeath(final PlayerDeathEvent event) {
+		// If the killer of the player is null, return and don't do anything.
+		// Writing the condition in this order also prevents event.getEntity().getKiller() from being assigned TO null, instead of throwing an error, JUST IN CASE you use a SINGLE EQUAL SIGN. :)
+		Player killer = event.getEntity().getKiller();
+		if (null == killer) return;
+
+		// Check the killer's currently equipped weapon
+		ItemStack handstack = killer.getEquipment().getItemInMainHand();
+
+		// Get the map of enchantments on that item
+		Map<Enchantment, Integer> itemEnchants = handstack.getEnchantments();
+		if (itemEnchants.isEmpty()) return;
+
+		// Check if one enchantment is Smite
+		if (null == itemEnchants.get(Enchantment.DAMAGE_UNDEAD)) return;
+		
+		
+		double randomNum = Math.random();
+		double levelOfSmite = handstack.getEnchantmentLevel(Enchantment.DAMAGE_UNDEAD);
+		double levelOfLooting = handstack.getEnchantmentLevel(Enchantment.LOOT_BONUS_MOBS);
+
+		// The actual percent drop chance algorithm, ripped straight out of ArthropodEggHack
+		double dropPercentage = (smiteChance * levelOfSmite) + (lootingChance * levelOfLooting);
+
+		// I might as well be code golfing at this point given the following one-liners:
+
+		boolean thresholdReached = randomNum < dropPercentage;
+		// If thresholdReached == true, use the heartItem. Else, use the meatItem.
+		Material chosenItem = thresholdReached ? heartItem : meatItem;
+		// Make a new Component.text(), and if thresholdReached == true, use the heartName. Else, use the meatName.
+		Component chosenName = Component.text(thresholdReached ? heartName : meatName);
+		// Make our chosenItem as a new ItemStack of 1 
+		ItemStack item = new ItemStack(chosenItem, 1);
+		// Paper counts the setDisplayName method as deprecated, so we'll use the new method.
+		ItemMeta meta = item.getItemMeta();
+		//ArrayList<String> lore = new ArrayList<String>();
+
+		// NOW WE HAVE ALL OF OUR STUFF. Take all of our STUFF and SMASH IT BACK IN.
+		lore.add();
+		item.getItemMeta()//.displayName(chosenName);
+		event.getDrops().add(item);
+
+		// TODO: Get lore from config, and if lore = null, return
+		// TODO: Create a replacer that removes defintions like %v and %k and replace them with the names of the Victim and Killer wherever defined, respectfully
+		// TODO: Append lore to heartItem
+
+		// if (randomNum < targetPercentage) {
+		// 	ItemStack item = new ItemStack(heartItem, 1);
+		// 	event.getDrops().add(item);
+		// } else {
+		// 	ItemStack item = new ItemStack(meatItem, 1);
+		// 	event.getDrops().add(item);
+		// }
+
+	}
+}
